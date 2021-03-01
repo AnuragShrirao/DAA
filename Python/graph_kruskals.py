91034f26716ddd4e5eaf3111fc512e68e55c423c
AnuragShrirao <anuragshrirao@gmail.com>
2021-03-01 23:47:44 +0530

    prims and kruskal algorithm

---
+++ Python/graph_kruskals.py
@@ -0,0 +1,74 @@
+import time
+from collections import defaultdict
+
+class Graph:
+    def __init__(self, V):
+        self.V = V
+        self.graph = []
+    def addEdge(self, u, v, w):
+        self.graph.append([u, v, w])
+    def find(self, parent, i):
+        if parent[i] == i:
+            return i
+        return self.find(parent , parent[i])
+    def union(self, parent, rank, x, y):
+
+        xroot = self.find(parent , x)
+        yroot = self.find(parent , y)
+        
+        if rank[xroot] < rank[yroot]:
+            parent[xroot] = yroot
+
+        elif rank[xroot]> rank[yroot]:
+            parent[yroot] = xroot
+
+        else:
+            parent[yroot] = xroot
+            rank[xroot] +=1
+
+    def KruskalMST(self):
+        i = 0
+        e = 0
+        parent =[]
+        rank =[]
+        result = []
+        self.graph = sorted(self.graph , key= lambda item:item[2])
+        for node in range(self.V):
+            parent.append(node)
+            rank.append(0)
+       
+        while e< self.V-1:
+            u, v, w = self.graph[i]
+            i = i+1
+            x = self.find(parent, u)
+            y = self.find(parent, v)
+
+            if x != y:
+                e = e+1
+                result.append([u, v, w])
+                self.union(parent, rank, x, y)
+
+        print(f"Edges in Kruskal's MST are")
+        minCost = 0
+        for u, v, w in result:
+            print("%d -- %d == %d"%(u,v,w))
+            minCost += w
+
+        print("Minimum cost =",minCost)
+
+n = int(input("Enter number of vertices of the graph "))
+g = Graph(n)
+x = int(input("Enter number of edges in the graph "))
+arr = [None]* 3
+for i in range(x):
+    print("Enter starting vertex, ending vertex and weight of edge {} ".format(i+1))
+    for j in range(3):
+        arr[j] = int(input())
+    g.addEdge(arr[0], arr[1], arr[2])
+
+start = time.time()
+g.KruskalMST()
+end = time.time()
+print(end = "\n")
+print(f"Runtime of Kruskal's algorithm is {end-start}")
+            
---
+++ Python/graph_prims.py
@@ -0,0 +1,81 @@
+import sys
+import time
+
+class Graph:
+    def __init__(self,vertices):
+        self.V = vertices
+        self.graph = [[0 for row in range(vertices)]for column in range(vertices)]
+
+    def printMST(self, parent):
+        minCost = 0
+        print("\nThe edges in minimum spanning tree of given graph are\n")
+        print("Edge\tWeight")
+        for i in range(1, self.V):
+            print(parent[i], "--", i,"\t" , self.graph[i][parent[i]])
+            minCost += self.graph[i][parent[i]]
+
+        print(f"Minimum cost of spanning tree by Prim's algorithm is =", minCost)
+
+    def minKey(self, key, mstSet):
+
+        min = sys.maxsize
+
+        for v in range(self.V):
+            if key[v]< min and mstSet[v] == False:
+                min = key[v]
+                min_index = v
+
+        return min_index
+
+    def primMST(self):
+
+        key = [sys.maxsize] * self.V
+        parent = [None]* self.V
+        mstSet = [False]* self.V
+        key[0] = 0
+        parent[0] = -1
+
+        for x in range(self.V):
+            u = self.minKey(key, mstSet)
+
+            mstSet[u] = True
+
+            for v in range(self.V):
+                if self.graph[u][v] > 0 and key[v]> self.graph[u][v] and mstSet[v] == False:
+                    key[v] = self.graph[u][v]
+                    parent[v] = u
+
+        self.printMST(parent)
+
+n = int(input("Enter number of vertices of the graph "))
+g = Graph(n)
+# g = Graph(4)
+print("Enter the adjacency matrix of the Graph \n")
+for i in range(n):
+    print("Enter", n ,"values in row",i+1)
+    for j in range(n):
+        g.graph[i][j]=int(input())
+
+##g.graph = [[0,10,6,5],
+##           [10,0,0,15],
+##           [6,0,0,4],
+##           [5,15,4,0]]
+
+start = time.time()
+g.primMST()
+end = time.time()
+print(end = "\n")
+print(f"Runtime of Prims's algorithm is {end-start}")
+
+
+
+
+
+
+
+
+
+
+
+
+        
